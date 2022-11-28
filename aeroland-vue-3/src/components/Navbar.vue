cb18b0f556f489be429782fe72bd7f3cb28a6e08
Shannen Nazareno <19103991@usc.edu.ph>
2022-11-29 03:41:02 +0800

    header:navbar (below logo/brand identifier)

--- aeroland-vue-3/src/components/Navbar.vue
+++ aeroland-vue-3/src/components/Navbar.vue
@@ -2,19 +2,6 @@
     <header class="header-area navbar-light fixed-top" :class="{'is-sticky': isSticky}">
         <div class="container-fluid container-fluid--cp-150">
             <nav class="navbar navbar-expand-lg" id="navbar-example2">
-                <router-link to="/">
-                    <img src="../assets/img/logo/logo-dark.png" alt="logo">
-                </router-link>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="header-config-wrapper btn-group">
-                    <button class="header-config btn" @click="toggleClass('addClass', 'active')">
-                        <i class="far fa-search"></i>
-                    </button>
-                    <router-link to="" class="ht-btn ht-btn--outline hire-btn d-none d-xl-block">Hire Us Now</router-link>
-                </div>
 
                 <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
                     <ul class="nav main-menu">
@@ -23,26 +10,67 @@
                                 <span>HOME</span>
                             </a>
                         </li>
+
                         <li class="nav-item">
                             <a class="nav-link" href="#about" @click="menuCollapseOnLinkClick">
-                                <span>ABOUT</span>
+                                <span>ABOUT US</span>    
+                                <div class= "dropdown about-us">
+                                    <a href="#">History</a>
+                                    <a href="#">Company Profile</a>
+                                    <a href="#">Financial Report</a>
+                                    <a href="#">Board of Directors</a>
+                                    <a href="#">Corporate Officers</a>
+                                    <a href="#">Awards & Distinction</a>
+                                </div> 
+    
                             </a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#requirement" @click="menuCollapseOnLinkClick">
-                                <span>REQUIREMENTS</span>
+                                <span>MEMBERSHIP</span>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#pricing" @click="menuCollapseOnLinkClick">
-                                <span>PRICING</span>
+                                <span>INVESTMENT</span>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#partner" @click="menuCollapseOnLinkClick">
-                                <span>PARTNERS</span>
+                                <span>HEALTH PLANS</span>
                             </a>
                         </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#partner" @click="menuCollapseOnLinkClick">
+                                <span>LOANS</span>
+                                <div class="dropdown loans">
+                                    <a href="#">Commmon Provisions</a>
+                                    <a href="#">Employee's Loan</a>
+                                    <a href="#">Special Individual Loans</a>
+                                    <a href="#">PAL - Personal Advantage Loan</a>
+                                    <a href="#">Commercial Loan</a>
+                                    <a href="#">MicroFinance Loans</a>
+                                    <a href="#">CFI Credit Cash Wallet</a>
+                                </div>
+                            </a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#partner" @click="menuCollapseOnLinkClick">
+                                <span>SUPPORT</span>
+                                <div class="dropdown support" style="width: 13rem">
+                                    <a href="#">CFI E-Services</a>
+                                    <a href="#">Help Desk</a>
+                                    <a href="#">Online Membership</a>
+                                    <a href="#">Download Forms</a>
+                                    <a href="#">CFI Videos</a>
+                                    <a href="#">Calculator</a>
+                                    <a href="#">Branch </a>
+                                </div>
+                            </a>
+                        </li>
+
                     </ul>
                 </div>
             </nav>
@@ -50,6 +78,39 @@
     </header>
 </template>
 
+<style>
+    .nav-item .dropdown{
+        display: none;
+        position: absolute;
+        box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+        z-index: 1;
+    }
+
+    .nav-item:hover .dropdown{
+        display:block;
+        z-index: 1;
+        background-color: white;
+    }
+
+    .nav-item:hover .dropdown a{
+        display: block;
+        z-index: 1;
+        padding: 2px 4px;
+        margin: 0px 16px 4px 10px;
+        background-color: white;
+        font-size: smaller;
+    }
+
+    .nav-item{
+        margin: 0rem 1.7rem 0rem 1.7rem;
+    }
+
+    #navbar-example2{
+        z-index: 1;
+    }
+   
+
+</style>
 <script>
     export default {
         data (){
