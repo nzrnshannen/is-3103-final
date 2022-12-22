1341a5f2a8a57f71e9f19d514f2ece7267761bed
Clint <clintenglis16@gmail.com>
2022-12-22 21:18:40 +0800

    2nd changes

---
+++ aeroland-vue-3/src/components/sections/CommonProvisions.vue
@@ -0,0 +1,140 @@
+<template>
+    <div class="product-showcase-video-wrapper section-space--pt_60 section-space--pb_120" id="investment">
+        <div class="container-fluid container-fluid--cp-120">
+            <div class="row align-items-center">
+                <div class="margin1">
+                    <div class="product-showcase-video video-popup video-link">
+                        <div >
+                            <img class="img-fluid" src="../../assets/img/cprovisions.jpg" alt="investment-image">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row pricing-table-four">
+        <div class="">
+            <div class="pricing-table__inner">
+                <div class="pricing-table__top">
+                    <div class="pricing-table__header">
+                        <div class="pricing-table__image">
+                            <img src="">
+                        </div>
+                        <h4 class="margin2" style="text-align:center; padding-right:10rem;">Common Provisions</h4>
+                        <br>
+                    </div>
+                    <h5 class="margin2">Application Form</h5>
+                    <div class="pricing-table__body">
+                        <p class="alignp1">Every qualified applicant must fill-up the form prescribed for the loan type applied form. This can take the form of an electronic form if done through the online services of the CFI or through a printed form which the applicant may submit directly through a CFI office or through any outlet of Mlhuillier. All the blank fields in the form must be filled up. Otherwise, the application will be denied.</p>
+                        <p class="alignp1">To download forms <a class="hover-style-link hover-style-link--green" href="https://www.cfiforms.com/copy-of-home-1">Click here</a></p>
+                    </div>
+                </div>
+            </div>
+         
+        </div>
+
+        <div class="">
+            <div class="pricing-table__inner">
+                <div class="pricing-table__top">
+                    <div class="pricing-table__header">
+                        <div class="pricing-table__image">
+                            <img src="">
+                        </div>
+                        <h5 class="margin4">Payment Due Date</h5>
+                    </div>
+                    <div class="pricing-table__body">
+                       <p class="alignp1">For loans payable by installment, the first installment is due on the 8th day of the month following the date of approval of the application.  For example: if the loan was approved on February 15, the first installment payment is due on March 8.</p>
+                       
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+        <div class="">
+            <div class="pricing-table__inner">
+                <div class="pricing-table__top">
+                    <div class="pricing-table__header">
+                        <div class="pricing-table__image">
+                            <img src="">
+                        </div>
+                        <h5 class="margin4">Payment Period</h5>
+                    </div>
+                    <div class="pricing-table__body">
+                        <p class="alignp1">A borrower has thirty ( 30) days from the due date to pay the amount due for the month.</p>
+                        
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+
+        <div class="">
+            <div class="pricing-table__inner">
+                <div class="pricing-table__top">
+                    <div class="pricing-table__header">
+                        <div class="pricing-table__image">
+                            <img src="">
+                        </div>
+                        <h5 class="margin4">Past Due or in Arrears</h5>
+                    </div>
+                    <div class="pricing-table__body">
+                        <p class="alignp1">A loan is considered past due or in arrears if the amount due for the current month is not paid within a period of 30 days from the due date. In such an eventuality, any amount considered past due will incur a penalty equivalent to 2% for every month of delayed payment.</p>
+                        
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+
+.margin1{
+    margin-left: 6.2rem;
+    margin-top: 10rem;
+    margin-bottom: -7rem;
+}
+
+.margin2{
+    padding-bottom:0rem;
+    text-align:left;
+    margin-left:12.5rem;
+    margin-top:-1rem;
+   
+}
+
+.margin3{
+    padding-right:10px;
+    padding-bottom:5px;
+}
+
+.margin4{
+    margin-top: -5.5rem;
+    text-align:left;
+    margin-left:12.5rem;
+}
+
+.alignleft{
+    text-align: left;
+}
+
+.alignp{
+    text-align:justify;
+    padding-left:1.5rem;
+    padding-right:1.5rem;
+}
+
+.alignp1{
+    text-align:justify;
+    padding-left:12.5rem;
+    padding-right:12.5rem;
+    font-size: 19px;
+}
+
+.alignp2{
+    text-align:left;
+    padding-left:1.5rem;
+}
+
+</style>
